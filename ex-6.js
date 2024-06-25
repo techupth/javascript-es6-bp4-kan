2cea4c1e47d6baf6a787b2f62ba8fcbb14e9ab8d
Warat <warat.sat@gmail.com>
2024-06-25 17:20:59 +0700

    Update assignment

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,7 @@
 let height = undefined;
 let result;
-
+function valueCheck(height){
+   return height=undefined?? 'Height is not defined'
+}
+result=valueCheck(height)
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
